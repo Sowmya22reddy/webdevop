9eed27673481c92f026f34857cfa7134beffac9b
ReddySowmya <reddysowmya2293@gmail.com>
2019-04-21 10:58:54 -0700

    initial commit

---
+++ TV Remote/remote.js
@@ -0,0 +1,48 @@
+function onFunc() {
+    var powerOn = document.getElementById('channel');
+    powerOn.innerHTML = "Power ON";
+}
+function offFunc() {
+    var powerOff = document.getElementById('channel');
+    powerOff.innerHTML = "Power OFF";
+}
+function channel7() {
+    var channel_7 = document.getElementById('channel');
+    channel_7.innerHTML = "Channel No. is 7";
+}
+function channel8() {
+    var channel_8 = document.getElementById('channel');
+    channel_8.innerHTML = "Channel No. is 8";
+}
+function channel9() {
+    var channel_9 = document.getElementById('channel');
+    channel_9.innerHTML = "Channel No. is 9";
+}
+function channel4() {
+    var channel_4 = document.getElementById('channel');
+    channel_4.innerHTML = "Channel No. is 4";
+}
+function channel5() {
+    var channel_5 = document.getElementById('channel');
+    channel_5.innerHTML = "Channel No. is 5";
+}
+function channel6() {
+    var channel_6 = document.getElementById('channel');
+    channel_6.innerHTML = "Channel No. is 6";
+}
+function channel1() {
+    var channel_1 = document.getElementById('channel');
+    channel_1.innerHTML = "Channel No. is 1";
+}
+function channelTwo() {
+    var channel_2 = document.getElementById('channel');
+    channel_2.innerHTML = "Channel No. is 2";
+}
+function channelThree() {
+    var channel_3 = document.getElementById('channel');
+    channel_3.innerHTML = "Channel No. is 3";
+}
+function channelTen() {
+    var channel_10 = document.getElementById('channel');
+    channel_10.innerHTML = "Channel No. is 10";
+}
---
+++ TV Remote/remote.ts
@@ -0,0 +1,85 @@
+
+
+function onFunc(){
+    
+    let powerOn = document.getElementById('channel') as HTMLInputElement;
+    powerOn.innerHTML = "Power ON";
+
+}
+
+function offFunc(){
+    
+    let powerOff = document.getElementById('channel') as HTMLInputElement;
+    powerOff.innerHTML = "Power OFF";
+
+}
+
+function channel7(){
+    
+    let channel_7 = document.getElementById('channel') as HTMLInputElement;
+    channel_7.innerHTML = "Channel No. is 7";
+
+}
+
+function channel8(){
+    
+    let channel_8 = document.getElementById('channel') as HTMLInputElement;
+    channel_8.innerHTML = "Channel No. is 8";
+
+}
+
+function channel9(){
+    
+    let channel_9 = document.getElementById('channel') as HTMLInputElement;
+    channel_9.innerHTML = "Channel No. is 9";
+
+}
+
+function channel4(){
+    
+    let channel_4 = document.getElementById('channel') as HTMLInputElement;
+    channel_4.innerHTML = "Channel No. is 4";
+
+}
+
+function channel5(){
+    
+    let channel_5 = document.getElementById('channel') as HTMLInputElement;
+    channel_5.innerHTML = "Channel No. is 5";
+
+}
+
+function channel6(){
+    
+    let channel_6 = document.getElementById('channel') as HTMLInputElement;
+    channel_6.innerHTML = "Channel No. is 6";
+
+}
+
+function channel1(){
+    
+    let channel_1 = document.getElementById('channel') as HTMLInputElement;
+    channel_1.innerHTML = "Channel No. is 1";
+
+}
+
+function channelTwo(){
+    
+    let channel_2 = document.getElementById('channel') as HTMLInputElement;
+    channel_2.innerHTML = "Channel No. is 2";
+
+}
+
+function channelThree(){
+    
+    let channel_3 = document.getElementById('channel') as HTMLInputElement;
+    channel_3.innerHTML = "Channel No. is 3";
+
+}
+
+function channelTen(){
+    
+    let channel_10 = document.getElementById('channel') as HTMLInputElement;
+    channel_10.innerHTML = "Channel No. is 10";
+
+}
